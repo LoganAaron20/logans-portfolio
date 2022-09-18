714ba2d5fce0b16ecfdbfc60e74b09ce13cd7650
Logan Aaron <loganaaron16@gmail.com>
2022-09-18 01:48:11 -0500

    Started contact form and styling

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,13 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import "./contact.styles.css";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+
+  const onSubmit = () => {
+    console.log("Submitted");
+  };
+
+  const nameChange = (e) => {
+    setName(e.target.value);
+    console.log("NAME", name);
+  };
+
+  const emailChange = (e) => {
+    setEmail(e.target.value);
+    console.log("EMAIL", email);
+  };
+
+  const numberChange = (e) => {
+    setNumber(e.target.value);
+    console.log("NUMBER", number);
+  };
+
   return (
-    <div>
-      <Navbar />
-      <Footer />
-    </div>
+    <>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="body">
+        <div className="contact-form">
+          <form onSubmit={onSubmit}>
+            <div className="input">
+              <label htmlFor="name">Name:</label>
+              <br />
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={nameChange}
+                placeholder="Enter your full name"
+                required
+              />
+            </div>
+            <br />
+            <div className="input">
+              <label htmlFor="email">Email:</label>
+              <br />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={emailChange}
+                placeholder="Enter your email"
+                required
+              />
+            </div>
+            <br />
+            <div className="input">
+              <label htmlFor="number">Phone Number:</label>
+              <br />
+              <input
+                type="text"
+                id="phone_number"
+                name="phone_number"
+                value={number}
+                onChange={numberChange}
+                placeholder="XXX-XXX-XXXX"
+                required
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
